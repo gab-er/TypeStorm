46b51f826e53c1fbba15dc4dc4f9ab0e1537a90c
Gabriel Er <gabrielerkianrong@gmail.com>
2025-05-26 19:18:57 +0800

    Fixed TypeStorm Logo

--- frontend/src/app/components/Navbar.jsx
+++ frontend/src/app/components/Navbar.jsx
@@ -53,9 +53,9 @@ const Navbar = () => {
             </DisclosureButton>
           </div>
           {/* Team Logo */}
-          <LinkBox href="/" width="w-48" height="h-48">
-            <Logo />
-          </LinkBox>
+          <Link href='/'> 
+            <Logo width="120" height="120"/>
+          </Link>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex shrink-0 items-center"></div>
             <div className="hidden sm:ml-6 sm:block">
