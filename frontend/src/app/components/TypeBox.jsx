64ae5085e14893e52bce212fc30af80f0eab46aa
Jeremy Lee <jeremyleezhe@gmail.com>
2025-05-27 16:03:20 +0800

    Added Placeholder Typebox

---
+++ frontend/src/app/components/TypeBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TypeBox = () => {
+  return (
+    <div style = {{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '500px'}}
+    display >
+        <input style = {{height: '50%', width:'50%', textAlign: "center"}}placeholder = "Placeholder Typing Bar"/>
+    </div>
+  )
+}
+
+export default TypeBox
