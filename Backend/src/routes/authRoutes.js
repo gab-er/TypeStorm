02b0e3008ff6de7e2566f67ccc2300e7f1bb42b0
Jeremy Lee <jeremyleezhe@gmail.com>
2025-05-30 15:22:44 +0800

    added partitioned attribute to cookie

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -46,7 +46,8 @@ router.post('/login', async (req, res) => {
         const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: '24h'})
         res.cookie('jwt', token, {
             httpOnly : true,
-            maxAge: 24 * 60 * 60 * 1000  
+            maxAge: 24 * 60 * 60 * 1000,
+            partitioned : true 
         })
         res.send({message:`Successfully Authenticated ${username}`})
     } catch(err) {
