fd52ced55e8ee7e7e7e54b2b74d4fd04d02c94cd
Gabriel Er <gabrielerkianrong@gmail.com>
2025-06-13 21:42:05 +0800

    reset game on leaving page

--- frontend/src/app/components/TypeBox/TypeBox.jsx
+++ frontend/src/app/components/TypeBox/TypeBox.jsx
@@ -35,6 +35,10 @@ const TypeBox = () => {
   // Randomize the wordsData on first component mount
   useEffect(() => {
     setWordsToType(generateRandomWords(wordsData, WORDS_TO_TYPE));
+
+    return () => {
+      resetGame();
+    };
   }, []);
 
   // Events that will start/stop the timer
