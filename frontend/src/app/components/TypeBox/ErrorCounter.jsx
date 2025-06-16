6d3b7079e8b266223cbd563c65589c4cbd3f9935
Gabriel Er <gabrielerkianrong@gmail.com>
2025-06-16 11:44:08 +0800

    reformatted code

--- frontend/src/app/components/TypeBox/ErrorCounter.jsx
+++ frontend/src/app/components/TypeBox/ErrorCounter.jsx
@@ -1,5 +1,6 @@
 const ErrorCounter = ({ errors }) => {
-  return <div className="text-2xl text-red-400">{errors}</div>;
+  return;
+  <div className="text-2xl text-red-400">{errors}</div>;
 };
 
 export default ErrorCounter;
