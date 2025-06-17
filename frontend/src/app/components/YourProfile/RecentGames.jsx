602ff7ed8cd086d6d905ac5f20f6d53625c4cfaf
Jeremy Lee <jeremyleezhe@gmail.com>
2025-06-17 15:13:18 +0800

    restructured recent games

--- frontend/src/app/components/YourProfile/RecentGames.jsx
+++ frontend/src/app/components/YourProfile/RecentGames.jsx
@@ -1,8 +1,9 @@
 
 import useGameStore from "@/app/stores/useGameStore";
-import Link from "next/link";
 import { useState } from "react";
 import GameDisplay from "./GameDisplay";
+import ShowMore from "./ShowMore";
+import HistoryLink from "./HistoryLink";
 
 const RecentGames = () => {
     // Get games from gamestore
@@ -17,10 +18,8 @@ const RecentGames = () => {
             <div className="flex justify-center items-center gap-8 text-center p-4">
                 <div className='p-1 w-8'></div>
                 {/*Button switches between diplaying 5 and 25 games*/}
-                <button className="font-semibold text-indigo-600 hover:text-indigo-500" onClick={display == 5?()=>setDisplay(25): ()=>setDisplay(5)}>
-                  {display == 5? "Show More":"Show Less"}
-                </button>
-                <Link href="/history" className="font-semibold text-indigo-600 hover:text-indigo-500 "> Full History </Link>
+                <ShowMore less = {5} more = {25} display = {display} setDisplay={setDisplay}/>
+                <HistoryLink/>
             </div>
             <div className="p-2"></div>
 
