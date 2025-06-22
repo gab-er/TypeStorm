f5b209fb207cb552671f62eff303db48ae628b48
Jeremy Lee <jeremyleezhe@gmail.com>
2025-06-22 21:30:04 +0800

    Added more robust error handling. Added tests for all endpoints used by frontend

--- Backend/src/middleware/authMiddleware.js
+++ Backend/src/middleware/authMiddleware.js
@@ -1,25 +1,24 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 // Middleware to check if User has valid token
 
-function authMiddleware(req,res,next) {
-    const cookie = req.cookies['jwt']
-    // return 401 if no token
-    if (!cookie) {
-        return res.status(401).send({message:"Missing Token"})
-    }
-    // Verify Cookie
-    jwt.verify(cookie, process.env.JWT_SECRET, (err, decoded) => {
-
+function authMiddleware(req, res, next) {
+  const cookie = req.cookies["jwt"];
+  // return 401 if no token
+  if (!cookie) {
+    return res.status(401).send({ message: "Missing Token" });
+  }
+  // Verify Cookie
+  jwt.verify(cookie, process.env.JWT_SECRET, (err, decoded) => {
     // return 401 if token is invalid
-    if (err) { 
-        return res.status(401).json({ message: "Invalid token" }) 
+    if (err) {
+      return res.status(401).json({ message: "Invalid Token" });
     }
 
     // Set userId as part of the request
-    req.userId = decoded.id
+    req.userId = decoded.id;
 
-    next()
-    })
+    next();
+  });
 }
 
-export default authMiddleware
+export default authMiddleware;
