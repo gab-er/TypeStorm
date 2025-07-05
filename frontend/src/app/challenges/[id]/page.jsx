19fee9bfc59ade1a9c8c921916257e51cf12154e
Gabriel Er <gabrielerkianrong@gmail.com>
2025-07-05 13:59:24 +0800

    added back button

--- frontend/src/app/challenges/[id]/page.jsx
+++ frontend/src/app/challenges/[id]/page.jsx
@@ -1,5 +1,7 @@
 import challenges from "@/lib/challenges";
 import ChallengeWrapper from "@/app/components/Challenge/ChallengeWrapper";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import Link from "next/link";
 
 export default async function ChallengeID({ params }) {
   const { id } = await params;
@@ -9,6 +11,13 @@ export default async function ChallengeID({ params }) {
   // This wrapper will set the mode of the game, and allows this page to continue being a server component
   return (
     <>
+      <div className="relative flex justify-start items-center">
+        <Link href="/challenges">
+          <div className="absolute flex mt-2 ml-2">
+            <ArrowBackIcon /> Back
+          </div>
+        </Link>
+      </div>
       <div className="relative">
         <div className="absolute w-full flex justify-center mt-10 text-xl">
           {challengeDescription}
