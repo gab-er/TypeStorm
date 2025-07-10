0495558f80e03c359b14eb82bc5ff6223451d530
Jeremy Lee <jeremyleezhe@gmail.com>
2025-07-10 14:42:02 +0800

    added challenge mode functionality

---
+++ frontend/src/lib/mockchallenge.js
@@ -0,0 +1,19 @@
+const challenges = [
+  {
+    text: "blah",
+    description: "blah",
+    title: "0. Man in the Arena",
+  },
+  {
+    text: "blah",
+    description: "blah",
+    title: "1. We Choose to go to the Moon",
+  },
+  {
+    text: "blah",
+    description: "blah",
+    title: "2. I Have a Dream",
+  },
+];
+
+export default challenges;
