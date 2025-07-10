c6c98027b745d226e8ddb5b90ab36ab7da73fdcc
Gabriel Er <gabrielerkianrong@gmail.com>
2025-07-10 21:32:06 +0800

    many ui changes

--- frontend/src/app/components/Navbar/LogoutButton.jsx
+++ frontend/src/app/components/Navbar/LogoutButton.jsx
@@ -6,7 +6,7 @@ const LogoutButton = () => {
 
   return (
     <button
-      className="cursor-pointer text-center block w-full px-3 py-1.5 text-sm text-gray-700 hover:bg-gray-100 data-focus:outline-hidden"
+      className="cursor-default text-center block w-full px-3 py-1.5 text-gray-600 hover:text-white data-focus:outline-hidden text-xl"
       onClick={() => logout(router)}
     >
       Sign out
