1c5ab30f5c89e781c154932965a50b0b49d483af
Gabriel Er <gabrielerkianrong@gmail.com>
2025-07-10 14:23:01 +0800

    removed old challenges

--- frontend/src/lib/challenges.js
+++ frontend/src/lib/challenges.js
@@ -1,21 +1,3 @@
-// const challenges = [
-//   "I have a dream that one day every valley shall be exalted, every hill and mountain shall be made low, the rough places will be made plain, and the crooked places will be made straight; and the glory of the Lord shall be revealed, and all flesh shall see it together.",
-
-//   "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness.",
-
-//   "The only thing we have to fear is fear itself - nameless, unreasoning, unjustified terror which paralyzes needed efforts to convert retreat into advance. In every dark hour of our national life, a leadership of frankness and vigor has met with that understanding and support of the people themselves.",
-
-//   "Success is not final, failure is not fatal: it is the courage to continue that counts. Never give in - never, never, never, never - in nothing, great or small, large or petty, never give in except to convictions of honor and good sense. Courage is rightly esteemed the first of human qualities.",
-
-//   "That's one small step for a man, one giant leap for mankind. As we explore the unknown, we must remember how far we've come - from wooden rockets to space stations, from primitive tools to digital revolutions. Humanity's journey is driven by bold ideas and the courage to act on them.",
-
-//   "You may encounter many defeats, but you must not be defeated. In fact, it may be necessary to encounter the defeats so you can know who you are, what you can rise from, and how you can still come out of it. Strength grows in moments when you think you can't go on.",
-
-//   "Government of the people, by the people, for the people, shall not perish from the earth. These honored dead gave their lives that a nation might live. It is for us, the living, to be dedicated to the unfinished work they have thus far so nobly advanced.",
-
-//   "All we have to decide is what to do with the time that is given us. Many that live deserve death. And some that die deserve life. Can you give it to them? Then do not be too eager to deal out death in judgment, even the very wise cannot see all ends.",
-// ].map((challenge) => challenge + " "); // Append a space to the end of every challenge item -> TypeBox takes this extra space into account in order to end a game
-
 const challenges = [
   {
     text: "Test",
