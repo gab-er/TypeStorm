d01b24f96a5f842d36ca400f0a7474dea76fddce
Gabriel Er <gabrielerkianrong@gmail.com>
2025-07-11 13:09:00 +0800

    removed capitalization of logout button

--- frontend/src/app/components/Navbar/LogoutButton.jsx
+++ frontend/src/app/components/Navbar/LogoutButton.jsx
@@ -9,7 +9,7 @@ const LogoutButton = () => {
       className="cursor-default text-center block w-full px-3 py-1.5 text-gray-600 hover:text-white data-focus:outline-hidden text-xl"
       onClick={() => logout(router)}
     >
-      Sign out
+      sign out
     </button>
   );
 };
