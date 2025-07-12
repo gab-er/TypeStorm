f783876aa8753c3acc63b2b8c532f13f7948d863
Gabriel Er <gabrielerkianrong@gmail.com>
2025-07-12 11:32:06 +0800

    ui changes

--- frontend/src/app/components/Navbar/LoginButton.jsx
+++ frontend/src/app/components/Navbar/LoginButton.jsx
@@ -6,7 +6,7 @@ const LoginButton = () => {
       href="/login"
       className="cursor-default text-white bg-gray-800 hover:bg-gray-900 font-medium rounded-full text-sm px-5 py-2.5 me-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
     >
-      Login
+      login
     </Link>
   );
 };
