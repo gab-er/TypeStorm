e06f2f351858919d0718f9f7f2c44b11f8113d06
Jeremy Lee <jeremyleezhe@gmail.com>
2025-08-09 00:28:00 +0800

    made changes the .enc.example

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,4 +2,4 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, console.log("Server is connected"));
+app.listen(PORT, console.log(`Server is listening on port ${PORT}`));
